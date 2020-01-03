596e1b86fc4b947f76806aba7cd81ae066971889
Gleb Kulik <gkulik@65apps.ml>
2020-01-03 21:32:17 +0300

    Initial Commit

---
+++ BazilPapers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BazilPapers
+//
+//  Created by Kulik on 1/3/20.
+//  Copyright © 2020 Kulik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
