3817c6f30153777eb5a6d9257965ad8b4efb26ac
GK <gleb.kulik@codabrasoft.com>
2020-10-11 23:03:10 +0300

    disable premium in menu

--- BazilPapers/UI/WallpapersList/WallpapersListViewController.swift
+++ BazilPapers/UI/WallpapersList/WallpapersListViewController.swift
@@ -29,7 +29,7 @@ class WallpapersListViewController: UIViewController {
         controller.appearance.popMenuFont = UIFont.systemFont(ofSize: 17, weight: .regular)
         controller.appearance.popMenuBackgroundStyle = .blurred(.dark)
         // Configure options
-        controller.shouldDismissOnSelection = false
+        controller.shouldDismissOnSelection = true
         controller.delegate = self
 
         // Present menu controller
@@ -51,7 +51,6 @@ class WallpapersListViewController: UIViewController {
 
     private func setupMenuDataSource() {
         self.menuItems = [
-            .init(title: "Premium", image:  #imageLiteral(resourceName: "get-premium"), color: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), type: .premiumAccess),
             .init(title: "Rate App", image: #imageLiteral(resourceName: "Star"), color:  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), type: .rate),
             .init(title: "Share", image: #imageLiteral(resourceName: "share"), color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), type: .share),
             .init(title: "Support", image: #imageLiteral(resourceName: "support"), color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), type: .support),
